6bfd499cda0d5c1a6ef9ef0ae6ae5e18c62a4a47
Ivan Pedrazas <ipedrazas@gmail.com>
2016-11-07 09:50:28 -0800

    test improved

--- receive/main_test.go
+++ receive/main_test.go
@@ -9,22 +9,31 @@ import (
 
 func TestHookHandler(t *testing.T) {
 
-	jsonprep := `{"name":"My Test","password":"Secret Password"}`
-	jsonStr := []byte(jsonprep)
-	data := bytes.NewBuffer(jsonStr)
-
-	request, err := http.NewRequest(
-		http.MethodGet,
-		"http://localhost:8080/",
-		data,
-	)
-	if err != nil {
-		t.Fatalf("Could not create request , %v", err)
+	cases := []struct {
+		in   string
+		code int
+	}{
+		{`{"name":"My Test","password":"Secret Password"}`, http.StatusOK},
+		{`{"name":"My Test","password": }`, http.StatusUnprocessableEntity},
+		{"", http.StatusUnprocessableEntity},
 	}
-	recorder := httptest.NewRecorder()
-	hookHandler(recorder, request)
-	if recorder.Code != http.StatusOK {
-		t.Errorf("Response is not 200 Ok -> %d", recorder.Code)
+
+	for _, c := range cases {
+		jsonStr := []byte(c.in)
+		data := bytes.NewBuffer(jsonStr)
+		request, err := http.NewRequest(
+			http.MethodGet,
+			"http://localhost:8080/",
+			data,
+		)
+		if err != nil {
+			t.Fatalf("Could not create request , %v", err)
+		}
+		recorder := httptest.NewRecorder()
+		hookHandler(recorder, request)
+		if recorder.Code != c.code {
+			t.Errorf("Response is not 200 Ok -> %d", recorder.Code)
+		}
 	}
 
 }
